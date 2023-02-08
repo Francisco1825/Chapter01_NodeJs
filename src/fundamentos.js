f72ba88c9b6e280a400bad19cfed169f0a877396
Francisco Douglas <fdouglas1825@gmail.com>
2023-02-08 14:55:44 -0300

    Finalizado fundamentos parâmetros Nessa primeira parte existe os methods http e como passar params, query e informações no body

--- src/fundamentos.js
+++ src/fundamentos.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
 app.get("/courses", (req, res) => {
   const query = req.query
   console.log(query)
@@ -9,6 +11,8 @@ app.get("/courses", (req, res) => {
 })
 
 app.post("/courses",(req, res)=>{
+  const body = req.body
+  console.log(body)
   return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"])
 })
 
